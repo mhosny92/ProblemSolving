1a4db4613acaad68c7e4423590348d0faf4d55ca
Mahmoud Hosny <mahmoud.h.abdallah@gmail.com>
2019-03-28 02:20:16 +0300

    Solving problem: http://codeforces.com/contest/807/problem/A

--- src/codeforces/is/it/rated/Main.java
+++ src/codeforces/is/it/rated/Main.java
@@ -1,4 +1,4 @@
-package forming.teams;
+package is.it.rated;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
@@ -6,56 +6,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.StringTokenizer;
-// This problem solves the Bipartite graph property.
+
+
+// my solution to problem http://codeforces.com/contest/807/problem/A
+
 public class Main {
     public static void main(String[] args) {
         MyScanner sc = new MyScanner();
         out = new PrintWriter(new BufferedOutputStream(System.out));
         // Start writing your solution here. -------------------------------------
         int n = sc.nextInt();
-        int k = sc.nextInt();
-        List<Boolean> visited = new ArrayList<Boolean>();
-        List<Integer> team = new ArrayList<Integer>();
-        List<List<Integer>> l = new ArrayList<List<Integer>>();
-        Integer cyclesNumber = 0;
-        for (int i = 0; i < n; i++) {
-            visited.add(false);
-            l.add(new ArrayList<Integer>());
+        List<Integer> l = new ArrayList<>();
+        int d = 0; // difference
+        for (int i=0; i<n; i++){
+            int b = sc.nextInt(); int a = sc.nextInt();
+            l.add(a);
+            d += Math.abs(a-b);
         }
-        for (int i = 0; i < k; i++) {
-            int t1 = sc.nextInt(), t2 = sc.nextInt();
-            l.get(t1 - 1).add(t2 - 1);
-            l.get(t2 - 1).add(t1 - 1);
-        }
-        for (int i = 0; i < l.size(); i++) {
-            if (!visited.get(i)) {
-                Ref<Integer> cnt = new Ref<>(0);
-                if (dfs(l, visited, i, -1, cnt)){
-                    cyclesNumber += cnt.value % 2 == 1? 1 : 0;
-                }
-            }
-        }
-        System.out.println((n - cyclesNumber) % 2 == 1 ? cyclesNumber+1 : cyclesNumber);
+        boolean sorted = l.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList()).equals(l);
+
+        if (d == 0 && sorted)
+            out.println("maybe");
+        else if (d == 0)
+            out.println("unrated");
+        else out.println("rated");
+
         // Stop writing your solution here. -------------------------------------
         out.close();
     }
-
-    private static boolean dfs(List<List<Integer>> l, List<Boolean> visited, int current, int parent, Ref<Integer> count) {
-        if (visited.get(current))
-            return true;
-        visited.set(current, true);
-        for (int j = 0; j < l.get(current).size(); j++) {
-            if (l.get(current).get(j) != parent) {
-                count.value++;
-                if (dfs(l, visited, l.get(current).get(j), current, count))
-                    return true;
-            }
-        }
-        return false;
-    }
-
     // -----------PrintWriter for faster output---------------------------------
     public static PrintWriter out;
 
@@ -103,12 +85,4 @@ public class Main {
 
     }
     // --------------------------------------------------------
-}
-
-class Ref<T>
-{
-    public T value;
-    public Ref(T v){
-        value = v;
-    }
 }
