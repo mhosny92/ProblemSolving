ca048c406bd3cf08619aef17f3cf0fa6386cd1c6
Mahmoud Hosny <mahmoud.h.abdallah@gmail.com>
2019-03-29 20:22:32 +0300

    Solving problems on GeeksForGeeks.com related to Amazon interview.

---
+++ src/geeksforgeeks/amazon/fulfillment/builder/Main.java
@@ -0,0 +1,33 @@
+package geeksforgeeks.amazon.fulfillment.builder;
+
+import general.heaps.minheap.MinHeap;
+
+import java.io.BufferedOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public  static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        PrintWriter out = out = new PrintWriter(new BufferedOutputStream(System.out));
+        //---------------------------------------- Solution ---------------------------
+        int n = scanner.nextInt();
+        long sum = 0;
+        Integer [] arr  = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        MinHeap<Integer> heap = new MinHeap<>(n, arr);
+        while(heap.size() > 1) {
+            int f = heap.poll();
+            int s = heap.poll();
+            int m = f+s;
+            sum += m;
+            heap.add(m);
+        }
+        out.println(sum);
+        //----------------------------------------    End   ----------------------------
+        out.close();
+
+    }
+}
